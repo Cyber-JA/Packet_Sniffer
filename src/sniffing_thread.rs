6c3c69bc2668a045ca0c0f167a07b47f6369ebc4
GiuseppeCaruso <s303335@studenti.polito.it>
2022-10-25 13:30:55 +0200

    Aggiunto multithreading, print del pacchetto, parsing per ethernet e header ipv4,6, arp

---
+++ src/sniffing_thread.rs
@@ -0,0 +1,29 @@
+use mpsc::Receiver;
+use std::sync::mpsc;
+use std::sync::mpsc::SyncSender;
+use std::thread;
+use pcap::{Device, Packet};
+use pktparse::ethernet::{EtherType, MacAddress};
+use pktparse::ip::IPProtocol;
+use report_packet::ReportPacket;
+use crate::parsing;
+use crate::report_packet::report_packet;
+
+//function used by the thread that must sniff packet
+pub fn sniff(net_adapter: usize, tx: SyncSender<ReportPacket>) -> () {
+
+    /****************** SNIFFING THREAD *******************/
+    thread::spawn(move || {
+        let list = Device::list().unwrap();
+        let mut cap = pcap::Capture::from_device(list[net_adapter-1].clone()).unwrap()
+            .promisc(true)
+            .open()
+            .unwrap();
+        while let Ok(packet) = cap.next_packet() {
+            let report = parsing::parse(packet);
+            tx.send(report).unwrap();
+        }
+
+    });
+    /******************************************************/
+}
