e5e0d991f04718c3552397134a5218ab115b6264
GiuseppeCaruso <s303335@studenti.polito.it>
2022-11-06 18:19:45 +0100

    added writing thread and writing on file. With this version, thread writes a packet each $timeout$ in specified file.

--- src/print_format.rs
+++ src/print_format.rs
@@ -2,6 +2,8 @@
 //TODO write format to print packet's information
 
 use std::fmt::{Display, Formatter};
+use std::fs::File;
+use std::io::Write;
 use pktparse::ethernet::MacAddress;
 use crate::report_packet::report_packet::ReportPacket;
 
@@ -11,5 +13,14 @@ pub fn print(packet: ReportPacket){
                 | {:?}:{:?} -> {:?}:{:?} | l3 protocol: {:?} | l4 protocol: {:?}", packet.source_mac.0[0], packet.source_mac.0[1], packet.source_mac.0[2], packet.source_mac.0[3], packet.source_mac.0[4], packet.source_mac.0[5],
              packet.dest_mac.0[0], packet.dest_mac.0[1], packet.dest_mac.0[2], packet.dest_mac.0[3], packet.dest_mac.0[4], packet.dest_mac.0[5]
              , packet.source_ip,packet.source_port,  packet.dest_ip, packet.dest_port, packet.l3_protocol, packet.l4_protocol);
+}
+
+pub fn fmt_for_file(packet: ReportPacket, file: &mut File){
+    let mut string = format!("{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}\
+            -> {:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x} \
+            | {:?}:{:?} -> {:?}:{:?} | l3 protocol: {:?} | l4 protocol: {:?}\n", packet.source_mac.0[0], packet.source_mac.0[1], packet.source_mac.0[2], packet.source_mac.0[3], packet.source_mac.0[4], packet.source_mac.0[5],
+                             packet.dest_mac.0[0], packet.dest_mac.0[1], packet.dest_mac.0[2], packet.dest_mac.0[3], packet.dest_mac.0[4], packet.dest_mac.0[5]
+                             , packet.source_ip,packet.source_port,  packet.dest_ip, packet.dest_port, packet.l3_protocol, packet.l4_protocol);
+    file.write_all(string.as_bytes());
 }
 
--- src/writing_thread.rs
+++ src/writing_thread.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::io::{BufReader, BufWriter};
 use mpsc::Receiver;
 use std::sync::mpsc;
@@ -10,17 +10,24 @@ use pcap::{Device, Packet};
 use pktparse::ethernet::{EtherType, MacAddress};
 use pktparse::ip::IPProtocol;
 use report_packet::ReportPacket;
-use crate::parsing;
-use crate::print_format::print;
+use crate::print_format::{print, fmt_for_file};
 use crate::report_packet::report_packet;
+use std::io::prelude::*;
+use std::path::PathBuf;
 
 pub fn write_file(file_name: String, timeout: u16, rx: Receiver<ReportPacket>){
     /****************** READING THREAD *******************/
     thread::spawn(move || {
+        let mut file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .append(true)
+            .create(true)
+            .open(file_name).unwrap(); //file opened in append mode, read-write mode, if not exists, create it
         loop {
             sleep(Duration::from_millis(timeout as u64));
-            println!("filename: {:?}, timeout: {:?}", file_name, timeout);
             let packet = rx.recv().unwrap();
+            fmt_for_file(packet, &mut file);
             print(packet);
         }
     });
